2f4c30fa848663a53bd53dfda29ea0fad48f0ea8
Jesting19514 <jestgx@gmail.com>
2024-08-27 08:40:43 -0600

    Se sube la actividad 1

---
+++ Automatas/src/automatas/Actividad_1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package automatas;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author team1
+ */
+public class Actividad_1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        int numeroEntero = 10;
+        float numeroDecimal = 10.5f ;
+        BigInteger numeroGrande = new BigInteger("12345678910111213141516");
+        String cadena = "Edgar Essaul";
+        System.out.println(cadena+(" ")+numeroEntero+(" ")+numeroDecimal+(" ")+numeroGrande);
+    } 
+}
