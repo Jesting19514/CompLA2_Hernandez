8cbf7cbe33a7225321fa5ff63b5514c0b9b53488
Jesting19514 <151665332+Jesting19514@users.noreply.github.com>
2024-09-02 08:16:01 -0600

    Se eliminan los archivos extras y se deja solo el proyecto
    perdon

--- CompLA2_Hernandez/src/compla2_hernandez/Principal.java
+++ CompLA2_Hernandez/src/compla2_hernandez/Principal.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package compiladorla2;
+package compla2_hernandez;
 
 /**
  *
  * @author team1
  */
-public class CompiladorLA2 {
+public class Principal {
 
     /**
      * @param args the command line arguments
